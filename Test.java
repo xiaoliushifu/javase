062a7ffae50283de640514f104d6bb9b208f2fae
liumingwei <liumingwei@yunzongnet.com>
2017-05-30 18:31:44 +0800

    需要从控制台里读取数据，所以使用了好多java的输入流，缓冲流,但是还没有包的概念，下面简单理解一下包的概念

--- Test.java
+++ Test.java
@@ -1,16 +1,34 @@
-//绫诲悕鏄疶est,鎵�浠ユ枃浠跺悕涔熷緱鏄疶est
 public class Test
 {
 	public static void main(String args[])
 	{
-		int a=3;
-		int b=4;
+//		int a=3;
+//		int b=4;
+
+		//输入流，好比是绑定了键盘一样
+		InputStreamReader isr =new InputStreamReader(System.in);
+
+		//缓冲区，从输入流读取的数据流，要先放到缓冲区
+		BufferedReader     br = new BufferedReader(isr);
+
+		//给出提示
+		System.out.println("请输入一个整数");
+		//读入一个整数
+		String a1 = br.readLine();
+
+		System.out.println("请输入二个整数");
+		String a2 = br.readLine();
+
+		//输入的数字都是字符串，需要强制转换给float类型  String-->Float
+		float a = Float.parseFloat(a1);
+		float b = Float.parseFloat(a2);
+
 		if (a==b) {
-			System.out.println("鐩哥瓑");
+			System.out.println("相等");
 		} else if (a>b) {
-			System.out.println("a澶т簬b");
+			System.out.println("a大于b");
 		} else {
-			System.out.println("a灏忎簬b");
+			System.out.println("a小于b");
 		}
 	}
 }
