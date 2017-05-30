9bf23cc809794c33f39a44557cd16996788469c5
liumingwei <liumingwei@yunzongnet.com>
2017-05-30 19:50:24 +0800

    java 循环

---
+++ Fortest.java
@@ -0,0 +1,27 @@
+public class Fortest
+{
+	public static void main(String arg[]) throws Exception
+	{
+		//for循环
+		/*for(int i=0;i<10;i++){
+			System.out.println("你好，我是刘德华");
+		}*/
+
+
+		//while循环
+		/*int i=0;
+		while(i<10) {
+			System.out.println("Hello,world");
+			i++;
+		}*/
+
+
+		//do......while循环
+		int i=0;
+
+		do{
+			System.out.println("世界，你好");
+			i++;
+		}while(i<10);
+	}
+}
