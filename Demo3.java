998bd9bf17e8c956133eb0190866c64c942c4ee9
liumingwei <liumingwei@yunzongnet.com>
2017-05-30 20:21:38 +0800

    更近一步，如何打印镂空的金字塔呢？

--- Demo3.java
+++ Demo3.java
@@ -2,8 +2,7 @@ public class Demo3
 {
 	public static void main(String arg[]) throws Exception
 	{
-		//死去火来发--打四层
-		int lay=4;
+		int lay=10;
 		for(int i=1;i<=lay;i++) {
 
 			//先打空格
@@ -12,12 +11,19 @@ public class Demo3
 				System.out.print(" ");
 			}
 
-			//每一层里打多少个*号
-			//第一行打印1个*;
-			//第二行打印3个*;
-			// 3---5    4---7
 			for(int j=1;j<=2*i-1;j++){
-				System.out.print("*");
+
+				//第一层和最后一层全部打*号
+				if(i==1 || i==lay) {
+					System.out.print("*");
+				//中间层，只打第一个和最后一个*号
+				} else if(j==1 || j==2*i-1) {
+					System.out.print("*");
+				//其他的打空格
+				} else {
+					System.out.print(" ");
+				}
+				
 			}
 			//打出一个换行
 			System.out.println();
@@ -25,3 +31,12 @@ public class Demo3
 		}
 	}
 }
+/*
+	中间镂空的金字塔，怎么打呢？
+          *
+		 * *
+		*   *
+	   *******
+	   我们观察，金字塔的顶端是完整的；金字塔的底端也是完整的。
+	   这中间的都是镂空的，所以，我们应该在打印*号的那个for循环中做文章
+*/
