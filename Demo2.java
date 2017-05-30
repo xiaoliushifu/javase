6c19ee4847278f52617c13019aa78e41663916b8
liumingwei <liumingwei@yunzongnet.com>
2017-05-30 19:05:01 +0800

    逻辑控制

---
+++ Demo2.java
@@ -0,0 +1,13 @@
+
+
+public class Demo2
+{
+	public static void main(String arg[])
+	{
+		int a=90; int b=90;
+		if(a==b|| a>b){System.out.println("ok1");}
+		b--;
+		if(a>b&& a>45){System.out.println("ok2");}
+		if (!(a<b)){System.out.println("ok3");}
+	}
+}
