5a06b4c39719263886de26e0b8fb5e91b89d8992
liumingwei <liumingwei@yunzongnet.com>
2017-05-30 18:14:20 +0800

    两个正数，判断，相等，大于，小于

---
+++ Test.java
@@ -0,0 +1,16 @@
+//类名是Test,所以文件名也得是Test
+public class Test
+{
+	public static void main(String args[])
+	{
+		int a=3;
+		int b=4;
+		if (a==b) {
+			System.out.println("相等");
+		} else if (a>b) {
+			System.out.println("a大于b");
+		} else {
+			System.out.println("a小于b");
+		}
+	}
+}
