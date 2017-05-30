05180ffe008fc09fdc11f6a4d47314d61774e14f
liumingwei <liumingwei@yunzongnet.com>
2017-05-30 18:47:17 +0800

    注意，try catch()语句，之间是完整的代码。有可能是一个作用域，这样catch写错位置的话，会报错，未定义的符号。切记代码应该写在try---catch之间。 java首次引入包，为了输入流，缓冲（区）流

--- Test.java
+++ Test.java
@@ -1,3 +1,6 @@
+//引入输入流InputStreamReader类所在的包
+import java.io.*;
+
 public class Test
 {
 	public static void main(String args[])
@@ -5,30 +8,37 @@ public class Test
 //		int a=3;
 //		int b=4;
 
-		//输入流，好比是绑定了键盘一样
-		InputStreamReader isr =new InputStreamReader(System.in);
+		try{
+			//输入流，好比是绑定了键盘一样
+			InputStreamReader isr =new InputStreamReader(System.in);
 
-		//缓冲区，从输入流读取的数据流，要先放到缓冲区
-		BufferedReader     br = new BufferedReader(isr);
+			//缓冲区，从输入流读取的数据流，要先放到缓冲区
+			BufferedReader     br = new BufferedReader(isr);
 
-		//给出提示
-		System.out.println("请输入一个整数");
-		//读入一个整数
-		String a1 = br.readLine();
+			//给出提示
+			System.out.println("请输入一个整数");
+			//读入一个整数
+			String a1 = br.readLine();
+			
 
-		System.out.println("请输入二个整数");
-		String a2 = br.readLine();
+			System.out.println("请输入二个整数");
+			String a2 = br.readLine();
 
-		//输入的数字都是字符串，需要强制转换给float类型  String-->Float
-		float a = Float.parseFloat(a1);
-		float b = Float.parseFloat(a2);
-
-		if (a==b) {
-			System.out.println("相等");
-		} else if (a>b) {
-			System.out.println("a大于b");
-		} else {
-			System.out.println("a小于b");
+			
+			//输入的数字都是字符串，需要强制转换给float类型  String-->Float
+			float a = Float.parseFloat(a1);
+			float b = Float.parseFloat(a2);
+			
+			
+			if (a==b) {
+				System.out.println("相等");
+			} else if (a>b) {
+				System.out.println("a大于b");
+			} else {
+				System.out.println("a小于b");
+			}
+		}catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
 	}
 }
