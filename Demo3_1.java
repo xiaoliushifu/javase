5f79951f26b47e70012ea570cdbf6977bce4cc03
liumingwei <liumingwei@yunzongnet.com>
2017-06-06 23:15:43 +0800

    java类的静态成员变量，也叫类成员变量 需要static关键字定义。使用时无需this.total使用。 类外部使用时，可以用对象.；也可以用类名.

--- Demo3_1.java
+++ Demo3_1.java
@@ -2,12 +2,21 @@ public class Demo3_1
 {
 	public static void main(String []args)
 	{
-		Dog dog1=new Dog(2,"大黄");
+		/*Dog dog1=new Dog(2,"大黄");
 		Person p1=new Person(dog1,23,"郭德纲");
 		p1.showInfo();
 		p1.dog.showInfo();
 		Person p2=new Person(dog1,24,"刘谦");
-		p2.showInfo();
+		p2.showInfo();*/
+
+		Dog d1=new Dog(2,"AA");
+		d1.joinGame();
+
+		Dog d2=new Dog(3,"BB");
+		d2.joinGame();
+
+		//d1.total或者d2.total或者Dog.total都能正常访问
+		System.out.println("有"+Dog.total+"个");
 	}
 }
 
@@ -39,6 +48,8 @@ class Dog
 {
 	int age;
 	String name;
+	//类成员变量
+	static int total;//静态变量
 	public Dog(int age,String name)
 	{
 		this.age=age;
@@ -48,5 +59,12 @@ class Dog
 	public void showInfo()
 	{
 		System.out.println("狗名是"+this.name);
+	}
+
+	public void joinGame()
+	{
+		//静态成员变量
+		total++;
+		System.out.println("有一个小狗来了");
 	}
 }
