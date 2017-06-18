68d1537f30dbefb6c8b861a6b107d135d366ebef
liumingwei <liumingwei@yunzongnet.com>
2017-06-18 17:34:18 +0800

    定义存放对象的数组 这种数组初始化时，各个元素的值都是null 重点又学习了从控制台读取数据的方法 InputStreamReader isr=new InputStreamReader(System.in); BufferReader br=new BufferReader(isr);
    上述的两行代码是固定的，写得时间长了就明白了

---
+++ Class5/src/com/test1/Demo5_2.java
@@ -0,0 +1,86 @@
+package com.test1;
+import java.io.*;
+
+public class Demo5_2 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		//初始化一个存放四只狗的对象数组，初始化时四个空间都是null
+		Dog dogs[]=new Dog[4];
+//		System.out.println(dogs[0]);
+		/*for(int i=0;i<dogs.length;i++) 
+		{
+			dogs[i]=new Dog();
+		}
+		dogs[0].setName("花花");
+		dogs[0].setWeight(4.5f);
+		*/
+		
+		//从控制台输入各个狗的信息
+		//isr好比和控制台绑定了一般
+		InputStreamReader isr=new InputStreamReader(System.in);
+		//由isr流对象从控制台读入的数据，缓冲给br对象，br是缓冲对象
+		BufferedReader br=new BufferedReader(isr);
+		for(int i=0;i<4;i++)
+		{
+			dogs[i]=new Dog();
+			System.out.println("请输入第"+(i+1)+"只狗的名字");
+			//从缓冲区里读取一行到代码的内存中，注意，这里需要处理IO异常。异常另说。
+			//程序将会在此处暂停，等待用户的输入 输入的数据都是字符串类型
+			String na=br.readLine();
+			dogs[i].setName(na);
+			
+			System.out.println("请输入第"+(i+1)+"只狗的体重");
+			String s_weight=br.readLine();
+			//字符串类型转成float类型 String=====>float
+			float weight = Float.parseFloat(s_weight);
+			dogs[i].setWeight(weight);
+		}
+		
+		//计算平均体重
+		//计算总体重
+		float allWeight=0;
+		for(int i=0;i<4;i++)
+		{
+			allWeight+=dogs[i].getWeight();
+		}
+		float avgWeight=allWeight/dogs.length;
+		System.out.println
+		("总体重: "+allWeight+" 平均体重: "+avgWeight);
+	}
+
+}
+
+//定义一个狗类
+class Dog
+{
+	private String name;
+	private float weight;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the weight
+	 */
+	public float getWeight() {
+		return weight;
+	}
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(float weight) {
+		this.weight = weight;
+	}
+	
+	
+}
