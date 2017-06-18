b5fee90c9063d31226e3b045453975b5adea4168
liumingwei <liumingwei@yunzongnet.com>
2017-06-18 10:46:33 +0800

    final的学习 final修饰方法，该方法在子类中不可被覆盖 final修饰成员变量，不影响继承，只在本类中初始化后不可更改 final修饰的类，不能被继承 注意final修饰的成员变量，多看看

--- First/src/com/mingwei/Demo3.java
+++ First/src/com/mingwei/Demo3.java
@@ -1,7 +1,9 @@
 /**
- * 功能：多态 
- * 一个类型在不同情况下的多种状态
- * 比如示例中的Animal类型
+ * 功能：final
+ * 修饰方法时，表示该方法不能被覆盖 
+ * 修饰类时，表示该类不可被继承
+ * 修饰变量时，子类继承时不能修改，但是子类仍然可以自己属于自己的成员变量，甚至和父类的final变量同名都没有问题。
+ * 如果你觉得某个方法或者某个类已经写得非常完美了，没有再修改的余地了
  */
 package com.mingwei;
 
@@ -9,140 +11,34 @@ public class Demo3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		/*
-		Cat c1=new Cat();
-		c1.cry();
-		Dog d1=new Dog();
-		d1.cry();
-		*/
-		
-		/*
-		//Animal是Cat的父类
-		Animal an=new Cat();
-		//此时an是cat类
-		an.cry();
-		an=new Dog();
-		//此时an又变成了Dog类
-		an.cry();
-		*/
-		//用最高级的父类声明an,这样它的子类都可以赋值给它，也就是说，an是个活指针，把Dog交给an，an就可以执行Dog的方法；把Cat给
-		//an,那么an就能执行Cat的方法。an不变，但是它的指向在改变。
-		
-		//问题：反过来可不可行呢？如下：
-		/*
-		 * Cat c1=new Animal();
-		 * 上述在什么条件下可以呢？
-		 * */
-		
-		Master m=new Master();
-		m.feed(new Dog(),new Bone());
-		
+		Bbb b=new Bbb();
+		System.out.println(b.rate);
+		b.rate=3.55f;
+		System.out.println(b.rate);
 	}
 
 }
 
-class Animal
+class Aaa
 {
-	int age;
-	String name;
-	/**
-	 * @return the age
-	 */
-	public int getAge() {
-		return age;
-	}
-	/**
-	 * @param age the age to set
-	 */
-	public void setAge(int age) {
-		this.age = age;
-	}
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-	//都会叫唤
-	protected void cry()
-	{
-		System.out.println("我是动物，不知道怎么叫唤");
-	}
-	
-	protected void eat()
-	{
-		System.out.println("不知道吃什么");;
-	}
-}
-
-//猫类
-class Cat extends Animal
-{
-	//覆盖父类的方法，方法覆盖 overide
-	public void cry()
-	{
-		System.out.println("miao miao");
-	}
-	
-	public void eat()
-	{
-		System.out.println("吃鱼");
-	}
-}
-
-//狗类
-class Dog extends Animal
-{
-	//覆盖父类的方法，方法覆盖 overide
-	public void cry()
-	{
-		System.out.println("wang wang");
-	}
-	
-	public void eat()
-	{
-		System.out.println("吃骨头");
-	}
-}
-
-class Master
-{
-	//喂食物
-	public void feed(Animal a,Food f)
-	{
-		a.eat();
-		f.showName();
-	}
-}
-
-class Food
-{
-	String name;
-	public void showName()
+	int a=0; //如果不给初值，a=0;这不是好习惯，可能会有的空指针
+	//final修饰的变量也不能被修改吗？
+	//这里的final修饰成员变量，不影响继承。子类完全可以定义自己与父类同名的成员变量，随意赋值不受其影响。
+	final float rate=3.1415926f;
+	//final修饰的方法 对继承有影响，子类不可覆盖该方法
+	public void sendMes()
 	{
 		
+		System.out.println("发送消息");
 	}
 }
-
-class Fish extends Food
+class Bbb extends Aaa
 {
-	public void showName()
+	//这里竟然可以初始化，父类Aaa的final怎么解释呢？
+	//final 修饰成员变量时，对继承没有影响
+	float rate=4;
+	public void sendMes()
 	{
-		System.out.println("鱼");
-	}
-}
-
-class Bone extends Food
-{
-	public void showName()
-	{
-		System.out.println("骨头");
+		
 	}
 }
