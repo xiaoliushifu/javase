774b48d0aadb39443b6b70f3831ed3c2cb1b5e7f
liushifu <872140945@qq.com>
2017-12-10 13:02:47 +0800

    这次增加了一个功能，非常重要： 	敌人坦克移动时，互相之间不能重叠（也就是碰撞） 如何实现呢？就是坐标不重叠呗！单拎出来一个敌人坦克分析， 就是当前坦克的坐标范围，不在队友坐标范围内即可。 根据当前坦克的朝向，队友坦克的朝向。总有8种情况，都写在了敌人坦克类里。 每次都判断当前坦克左上角和右上角是否在队友坐标范围内即可。 不在就是没有碰撞，在则是碰撞

---
+++ Class9/src/com/test4/Member.java
@@ -0,0 +1,572 @@
+/**
+ * 单独从原来一个java文件中拎出来
+ * 这个文件里单独定义坦克
+ * 
+ * 共性就是Tank
+ * 我的坦克就是Hero
+ * 敌人坦克就是Enemy。
+ * 
+ * 敌军的坦克有很多，数量不定，故应该使用集合而非数组。ArrayList,Vector都行。
+ * 敌军坦克是销毁和产生不断变化的，就选择了Vector。为啥不是ArrayList?
+ * 循环实例化敌军坦克，添加到Vector中
+ * 在paint方法里，从Vector中循环画出坦克（颜色，方向是共同的，可以在构造函数里初始化完成）
+ * 
+ * 坦克碰撞原理：
+ * 	总之一句话：当前坦克的坐标范围（比如左上角），是否在其他坦克的所占坐标的范围内，在即为碰撞；
+ *  根据当前坦克的方向，有以下这么几种情况：
+ *  	当前坦克朝上时：队友坦克有上下两个方向算一种情况；也有左右方向算一种情况；
+ *  	根据当天坦克的左上角坐标和右上角坐标，是否在队友坦克的坐标范围内即可。
+ *  当前坦克共有四种朝向，每种情况下队友都有两种情况，故4*2=8种情况
+ */
+
+package com.test4;
+
+//多个子弹对象
+import java.util.Vector;
+
+//坦克类
+class Tank
+{
+	
+	//坦克是否还有效（击中过了的坦克就是false）
+	boolean isLive = true;
+	//表示坦克横坐标
+	int x=0;
+	//纵坐标
+	int y=0;
+	//坦克方向
+	// 0 上  1 右   2下  3左
+	int direct = 0 ;
+	
+	//坦克颜色
+	int color = 0;
+	
+	
+	//坦克的速度（每次移动的像素数）
+	int speed = 1;
+	
+	//构造函数，初始化坦克所在的位置
+	public Tank(int x,int y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	/**
+	 * @return the x
+	 */
+	public int getX() {
+		return x;
+	}
+	/**
+	 * @param x the x to set
+	 */
+	public void setX(int x) {
+		this.x = x;
+	}
+	/**
+	 * @return the y
+	 */
+	public int getY() {
+		return y;
+	}
+	/**
+	 * @param y the y to set
+	 */
+	public void setY(int y) {
+		this.y = y;
+	}
+	/**
+	 * @return the direct
+	 */
+	public int getDirect() {
+		return direct;
+	}
+	/**
+	 * @param direct the direct to set
+	 */
+	public void setDirect(int direct) {
+		this.direct = direct;
+	}
+	
+	/**
+	 * @return the speed
+	 */
+	public int getSpeed() {
+		return speed;
+	}
+	/**
+	 * @param speed the speed to set
+	 */
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	/**
+	 * @return the color
+	 */
+	public int getColor() {
+		return color;
+	}
+	/**
+	 * @param color the color to set
+	 */
+	public void setColor(int color) {
+		this.color = color;
+	}
+}
+
+/**
+ * 敌人坦克
+ * @author Administrator
+ * 敌人坦克不受键盘控制，是独立运行的个体，故每个敌人坦克都是一个线程
+ * 
+ * 本次增加功能，实现敌人坦克移动时不可重叠的效果
+ *
+ */
+class Enemy extends Tank implements Runnable
+{
+	//敌人产生坦克的频率
+	int times=0;
+	//一个子弹，用来添加到子弹集合中
+	Shot s=null;
+	
+	//实现子弹连发，必须使用集合类
+	Vector<Shot> vs = new Vector<Shot>();
+	
+	//定义一个向量，可以访问到面板里所有敌人的坦克
+	Vector<Enemy> ves=new Vector<Enemy>();
+	
+	//得到面板里的所有敌人坦克
+	public void setVes(Vector<Enemy> ve)
+	{
+		this.ves = ve;
+	}
+	
+	public Enemy(int x, int y)
+	{
+		super(x,y);
+		//初始方向都朝下
+		this.setDirect(2);
+	}
+	
+	/**
+	 * 判断是否碰撞的方法,代码量多，但是逻辑较少
+	 * 熟悉原理，当前坦克的四个方向即可
+	 * @return
+	 */
+	public Boolean IsTouchOtherEnemy()
+	{
+		Boolean b=false;
+		
+		//分析碰撞的原理，就不在此处写了。这里是首先判断当前坦克的方向，再判断队友坦克的方向
+		//根据我的方向和队友方向的不同，有这么几种组合
+		switch(this.direct){
+		//当前坦克的方向朝上,左上角坐标是（x,y),右上角坐标是（x+20,y)
+		case 0:
+			//遍历队友坦克，判断当前坦克坐标和队友坦克坐标的重叠性呗
+			for(int i=0;i<ves.size();i++) {
+				//取出队友的坦克
+				Enemy e=ves.get(i);
+				
+				if(e!=this){//排除自己
+					//开始判断队友坦克的方向,朝上或者朝下
+					if(e.direct==0 || e.direct==2){
+						//如果当前坦克的左上角坐标范围在队友坦克的坐标范围内的话
+						if(this.x>=e.x && this.x<=e.x+20 && this.y>=e.y && this.y<=e.y+30) {
+							return true;
+						}
+						//判断当前坦克的右上角坐标范围是否在队友坦克的坐标范围内
+						if(this.x+20>=e.x && this.x+20<=e.x+20 && this.y>=e.y && this.y<=e.y+30){
+							return true;
+						}
+					}
+					//再判断队友的坦克方向，朝左或朝右
+					if(e.direct==1 || e.direct==3) {
+						//当前坦克的左上角
+						if(this.x>=e.x && this.x<=e.x+30 && this.y>=e.y && this.y<=e.y+20 ){
+							return true;
+						}
+						//当前坦克的右上角
+						if(this.x+20>=e.x && this.x+20 <=e.x+30 && this.y>=e.y && this.y<=e.y+20){
+							return true;
+						}
+					}
+				}
+			}
+			break;
+		case 1:
+			//当前坦克朝右边时，左上角坐标是(x+30,y),右上角坐标是（x+30,y+20)
+			for(int i=0;i<ves.size();i++) {
+				Enemy e=ves.get(i);
+				if(e!=this){
+					//开始判断队友坦克的方向,朝上或者朝下
+					if(e.direct==0 || e.direct==2){
+						//如果当前坦克的左上角坐标范围在队友坦克的坐标范围内的话
+						if(this.x+30>=e.x && this.x+30<=e.x+20 && this.y>=e.y && this.y<=e.y+30) {
+							return true;
+						}
+						//判断当前坦克的右上角坐标范围是否在队友坦克的坐标范围内
+						if(this.x+30>=e.x && this.x+30<=e.x+20 && this.y+20>=e.y && this.y+20<=e.y+30){
+							return true;
+						}
+					}
+					//再判断队友的坦克方向，朝左或朝右
+					if(e.direct==1 || e.direct==3) {
+						//当前坦克的左上角
+						if(this.x+30>=e.x && this.x+30<=e.x+30 && this.y>=e.y && this.y<=e.y+20 ){
+							return true;
+						}
+						//当前坦克的右上角
+						if(this.x+30>=e.x && this.x+30 <=e.x+30 && this.y+20>=e.y && this.y+20<=e.y+20){
+							return true;
+						}
+					}
+				}
+			}
+			break;
+		case 2:
+			//当前坦克朝下时，左上角坐标是（x+20,y+30），右上角坐标是（x,y+30）
+			for(int i=0;i<ves.size();i++) {
+				Enemy e=ves.get(i);
+				if(e!=this){
+					//开始判断队友坦克的方向,朝上或者朝下
+					if(e.direct==0 || e.direct==2){
+						//如果当前坦克的左上角坐标范围在队友坦克的坐标范围内的话
+						if(this.x+20>=e.x && this.x+20<=e.x+20 && this.y+30>=e.y && this.y+30<=e.y+30) {
+							return true;
+						}
+						//判断当前坦克的右上角坐标范围是否在队友坦克的坐标范围内
+						if(this.x>=e.x && this.x<=e.x+20 && this.y+30>=e.y && this.y+30<=e.y+30){
+							return true;
+						}
+					}
+					//再判断队友的坦克方向，朝左或朝右
+					if(e.direct==1 || e.direct==3) {
+						//当前坦克的左上角
+						if(this.x+20>=e.x && this.x+20<=e.x+30 && this.y+30>=e.y && this.y+30<=e.y+20 ){
+							return true;
+						}
+						//当前坦克的右上角
+						if(this.x>=e.x && this.x<=e.x+30 && this.y+30>=e.y && this.y+30<=e.y+20){
+							return true;
+						}
+					}
+				}
+			}
+			break;
+		case 3:
+			//当前坦克靠左时，左上角坐标为(x,y+20),右上角坐标（x,y)
+			for(int i=0;i<ves.size();i++) {
+				Enemy e=ves.get(i);
+				if(e!=this){
+					//开始判断队友坦克的方向,朝上或者朝下
+					if(e.direct==0 || e.direct==2){
+						//如果当前坦克的左上角坐标范围在队友坦克的坐标范围内的话
+						if(this.x>=e.x && this.x<=e.x+20 && this.y+20>=e.y && this.y+20<=e.y+30) {
+							return true;
+						}
+						//判断当前坦克的右上角坐标范围是否在队友坦克的坐标范围内
+						if(this.x>=e.x && this.x<=e.x+20 && this.y>=e.y && this.y<=e.y+30){
+							return true;
+						}
+					}
+					//再判断队友的坦克方向，朝左或朝右
+					if(e.direct==1 || e.direct==3) {
+						//当前坦克的左上角
+						if(this.x>=e.x && this.x<=e.x+30 && this.y+20>=e.y && this.y+20<=e.y+20 ){
+							return true;
+						}
+						//当前坦克的右上角
+						if(this.x>=e.x && this.x <=e.x+30 && this.y>=e.y && this.y<=e.y+20){
+							return true;
+						}
+					}
+				}
+			}
+		}
+		return b;
+	}
+	@Override
+	//敌人坦克作为一个线程，必须实现的run方法
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true){
+			try {
+				//每个坦克切换运动时休息50毫秒，也就是说敌人坦克将在1秒内切换20次
+				//这样初始看起来，敌人的坦克切换方向非常快，像个无头苍蝇乱转，没有平滑移动效果
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			//坦克方向移动，上右下左
+			switch(this.direct){
+				case 0:
+					//说明坦克正在向上移动。
+					//但是这样写效果有点问题，因为是移动一次，就去随机方向。在移动和切换方向之间转换非常快，
+					//故应该多移动，或者移动的时间长一些，切换方向的机会少一些，这样效果看起来好一点，平滑
+					//坦克很有可能只移动了一步，就改变了方向。看起来的效果就是无头苍蝇不停地切换方向，移动却很少，甚至不移动。
+					//于是让它多移动几步，并且休息一下
+					//移动多步，制造平滑效果
+					for(int i=0;i<10;i++) {
+						//控制坦克的移动范围在面板里
+						if(y>0 && !this.IsTouchOtherEnemy()) {
+							y-=speed;
+						}
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					break;
+				case 1:
+					for(int i=0;i<10;i++) {
+						//防止超过边界范围
+						if(x<400-30 && !this.IsTouchOtherEnemy()) {
+							x +=speed;
+						}
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					break;
+				case 2:
+					for(int i=0;i<10;i++) {
+						//控制坦克的移动范围在面板里
+						if(y<300-30 && !this.IsTouchOtherEnemy()) {
+							y +=speed;
+						}
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+					break;
+				case 3:
+					for(int i=0;i<10;i++) {
+						//控制坦克的移动范围在面板里
+						if(x>0 && !this.IsTouchOtherEnemy()) {
+							x -=speed;
+						}
+						try {
+							Thread.sleep(50);
+						} catch (InterruptedException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+			}
+			
+			/*下面的逻辑是处理敌人坦克如何产生子弹并发射，然后在渲染时才能看到效果*/
+			times++;
+			if(times%2 == 0){
+				//是否给坦克加入新的子弹
+				if(isLive){
+					//3说明可以连发2个子弹
+					if(vs.size()<3){
+						//没有子弹就要重新产生子弹（根据此时坦克的方向）
+						switch(direct){
+						case 0://上（注意，x坐标是坦克的坐上角）
+							s = new Shot(x+10,y,0);
+							break;
+						case 1://右
+							s = new Shot(x+30,y+10,1);
+							break;
+						case 2://下
+							s = new Shot(x+10,y+30,2);
+							break;
+						case 3://左
+							s = new Shot(x,y+10,3);
+							break;
+						}
+						//敌人的每颗子弹都是单独的一个进程
+						Thread ts=new Thread(s);
+						ts.start();
+						//添加到敌人的子弹集合里
+						vs.add(s);
+					}
+				}
+			}
+			//坦克不断移动，新方向随机产生
+			this.direct =(int)(Math.random()*4);
+			//判断敌人坦克是否死亡
+			if(!this.isLive){
+				break;
+			}
+		}
+	}
+}
+
+
+/**
+ * 我的坦克
+ * @author Administrator
+ *
+ */
+class Hero extends Tank
+{
+	//子弹也是java对象，是坦克的一个子对象
+	Shot s = null;
+	//实现子弹连发，必须使用集合类
+	Vector<Shot> vs = new Vector<Shot>();
+	
+	//子类必须写构造函数(因为父类没有定义默认的构造函数）
+	public Hero(int x,int y)
+	{
+		super(x,y);
+	}
+	
+	
+	//坦克具有发射子弹（开火）的能力
+	//但也不是随意创建，取决于当前坦克的方向及位置
+	//目前来做，坦克只有一个子弹
+	public void shotEnemy()
+	{
+		//根据坦克此时所处的方向，来实例化出子弹对象
+		switch(this.direct)
+		{
+		case 0://上（注意，x坐标是坦克的坐上角）
+			s = new Shot(x+10,y,0);
+			break;
+		case 1://右
+			s = new Shot(x+30,y+10,1);
+			break;
+		case 2://下
+			s = new Shot(x+10,y+30,2);
+			break;
+		case 3://左
+			s = new Shot(x,y+10,3);
+			break;
+		}
+		vs.add(s);
+		//启动子弹线程，每个子弹都是一个单独地进程
+		Thread t = new Thread(s);
+		t.start();
+	}
+	
+	//坦克向上移动，必须首先知道java面板里有一个坐标，以左上角为(0,0)原点，
+	//以此为基础的x，y变化（增大或减小）才能实现坦克的移动
+	public void moveUp()
+	{
+		y-=speed;
+	}
+	//坦克下移动
+	public void moveDown()
+	{
+		y+=speed;
+	}
+	//坦克向左移动
+	public void moveLeft()
+	{
+		x-=speed;
+	}
+	//坦克向右移动
+	public void moveRight()
+	{
+		x+=speed;
+	}
+	
+}
+
+/**
+ * 炸弹类，所谓爆炸类，就是执行那三张图片切换操作的一个类，它拥有着坦克爆炸时的坐标。
+ * 待有一辆坦克爆炸时，才需要实例化出来，按照一定顺序和频率，在某位置切换图片以达到爆炸效果，仅此而已。
+ * @author Administrator
+ *
+ */
+
+class Bomb
+{
+	//炸弹的坐标属性
+	int x,y;
+	//炸弹生命力
+	int life=9;
+	//炸弹是否激活 
+	boolean isLive=true;
+	public Bomb(int x,int y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+	
+	//减少生命值
+	public void lifeDown()
+	{
+		if (life>0) {
+			life--;
+		} else {
+			isLive=false;
+		}
+		
+	}
+	
+}
+
+
+
+//子弹类,以后会慢慢修改，增加功能
+//子弹一旦发射出来，就会按照初始的方向，直线运动
+//故子弹单独也是一个进程
+class Shot implements Runnable
+{
+	int x = 0;  //初始子弹x坐标
+	int y = 0;  //初始子弹y坐标
+	int direct =0 ; //子弹移动方向
+	int speed = 2; //子弹的移动速度
+	Boolean isLive = true;//子弹是否还在（击中坦克后，或者通过边界后就消失了）
+	public Shot(int x,int y,int direct)
+	{
+		this.x = x;
+		this.y = y;
+		this.direct = direct;
+	}
+	
+	public void run()
+	{
+		
+		//子弹一旦创建，就按照既定的方向，直线运动下去
+		while(true)
+		{
+			try {
+			//在敌人的坦克有多颗子弹时。注意：循环画出子弹的时机，要比渲染的频率要快。
+				//否则在渲染时，坦克尚未运动，这样即使画出了多颗子弹，子弹的坐标也是一样，也看不出子弹移动效果，
+				//看起来和一颗子弹是一样的效果。
+				//而且，子弹可以连发，但是不应该一直连发，可以让同一辆坦克的两颗子弹的间距随机起来
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			switch(direct)
+			{
+			case 0://上
+				y-=speed;
+				break;
+			case 1://上
+				x+=speed;
+				break;
+			case 2://上
+				y+=speed;
+				break;
+			case 3://上
+				x-=speed;
+				break;
+			}
+			//有一个问题，子弹对象何时消失退出内存呢？画子弹时判断是否有效，无效则退出内存
+			//direct.System.out.println("X坐标是"+x+" Y坐标是"+y);
+			
+			//判断该子弹是否到达面板的边缘
+			if (x<0 || x>400 ||y<0 || y>300) {
+				isLive = false;
+				break;
+			}
+		}
+	}
+}
---
+++ Class9/src/com/test4/MyTankGame4.java
@@ -0,0 +1,540 @@
+/**
+ * 功能： 坦克游戏 4.0
+ * 说明，先把坦克画出来
+ * 坦克可以上下左右移动
+ * 为窗口绑定事件监听者，事件监听者无须单独写，就是那个Panel。Panel自己监听其内部的组件
+ * 因为事件监听者就是要修改坦克的移动方向，而坦克就是从Panel中而来。故只需让Panel
+ * 实现事件监听者就行了，顺其自然
+ * 
+ * 使用KeyListener事件监听者接口。监听键盘按键w,d,s,a四个方向。
+ * 因为按键有个特点，只要按下，就会一直不断地触发按键事件（按着键不动和不断地点击按键是一个效果）
+ * 故在改变方向的同时，继续不断地触发按键事件，实现该方向上的移动（四个移动方法）x++,x--,y++,y--。
+ * 移动的速度，也可以单独定义一个变量speed。默认是1。
+ * 
+ * 除了上述的修改之外，坦克本身也得修改，尤其是指坦克的朝向（炮筒的朝向）所以得画出四个不同方向的坦克。
+ * 这个说来简单，但是要一点数学计算，只要在纸上画出四个朝向的坦克，自然就容易理解了
+ * 
+ * 这样上述几个步骤：   
+ * 	1实例化坦克对象，并初始化为坦克设置好方向与速度属性成员（尚未在面板对象里画出）
+ * 	2把坦克对象加入到面板里，并用每个坦克对象自身的方向和速度属性，在面板里画出具有各自朝向和初始位置的坦克
+ * 	3面板对象调用paint方法，在面板里就可以渲染出第2步画出的坦克来了
+ *  4不断地监听键盘事件（改变方向和移动的按键），调用repaint方法()重新渲染
+ *  5重复上述的第4步骤
+ *  
+ * 继续开发，这次加入了子弹对象 ，子弹是坦克对象的子对象，也是通过按键监听来产生的，按键就是通俗地发射子弹呗
+ * 子弹出现的位置也需要考虑一下，跟坦克炮筒的位置有关。
+ * 重要的思索：子弹也是进程，也需要渲染，子弹如何渲染呢？子弹在面板里画出的。故仍然需要面板来渲染
+ * 			这里所以也让面板实现了Runnable接口，使之脱离了只靠事件监听者来触发，然后repaint的限制。
+ * 
+ * 子弹可以发出来了，本次要实现的是子弹连发。
+ * 这个也简单，坦克对象使用一个集合来存储子弹对象。然后在paint方法里画子弹的位置
+ * 修改为遍历集合里的子弹对象，然后分别画出自己的x,y坐标及朝向即可。其他无需大改。
+ * 
+ * 子弹可以连发了，不能太霸道，目前限制为最多5发。
+ * 实现击中坦克的效果，击中的原理，就是子弹移动时的坐标，在坦克的坐标范围内就行。为此单独写一个方法hitTank()传入两个
+ * 对象，一个是子弹，一个是敌人坦克。
+ * 什么时候判断击中？或者何时调用hitTank()?。当然是无时不刻地判断了。所以，写在面板对象的run()方法里最合适。
+ * 目前的笨方法，就是取出子弹，每个子弹与现有的敌军坦克遍历比较。NxM乘法原理
+ * 
+ * 当坦克被击中时，增加爆炸效果,所谓爆炸效果，就是三张（甚至多张）效果图片的切换而已（动画片就是这个原理）
+ * 坦克被击中时，就要产生一个爆炸效果对象。由于面板是一个进程，无视不刻在画出面板里的元素，所以在面板里paint方法里
+ * 遍历爆炸效果集合，每个效果对象都各自处理那三张图片的切换，切换速度要考虑好。
+ * 可以为爆炸效果定义生命值，比如初始为9，每三个生命值切换一张图片，每次生命值减一直到生命值为0，爆炸效果对象消失
+ * 爆炸效果的产生和爆炸效果的展示，不在同一个逻辑里。
+ * 目前敌人坦克有三辆，但是第一个坦克（无论哪一个是第一个）被击中的爆炸效果不太明显，随后的两个倒是很明显，不知为何
+ * 
+ * 学习了文件IO对象后，替换三个图片文件对象imageIO，解决了第一个坦克没有爆炸效果的漏洞
+ * 
+ * 敌人的坦克可以随意移动，实现方式就是敌人的坦克也是一个线程
+ * 坦克的移动方向是随机的，坦克的移动步调可以控制，如果坦克每移动一步就切换方向，这样的效果看起来不太好；
+ * 所以，敌人坦克应该每移动多步后，再给一次切换坦克方向的机会（有可能方向相同），这样敌人坦克运行起来较为平滑
+ * 
+ * 控制敌人坦克的移动范围，应该在面板里。
+ * 这个简单，控制坦克的坐标在指定的范围内才移动，否则不移动。
+ * 
+ * 敌人可以发送子弹，且子弹的数量可控。
+ * 敌人的子弹的产生也是随机的，方案1：随着坦克的移动而产生，并不是一次循环产生。
+ * 方案2：放在paint里，写两个循环嵌套，循环每个坦克的每个子弹。数量不够就一直产生。（继续考虑）
+ * 坦克的死亡，是在画坦克时，如果坦克已经死亡就remove掉（坦克的子弹也就消失了）
+ * 子弹的死亡，也是在画坦克时判断
+ * 
+ * 我的坦克被击中时，也要消失。如何做到呢？
+ * 简单，就像判断我的子弹是否击中敌人坦克一样的逻辑；也分开判断敌人每个坦克的每个子弹，是否击中我就是了
+ * 
+ * 坦克的移动不能重叠
+ * 游戏进行的时候可以暂停，可以继续
+ * 线上玩家的成绩（打了多少坦克）
+ * 加上音效
+ * 
+ */
+package com.test4;
+
+import javax.imageio.ImageIO;//图片使用
+import java.io.*;//图片使用
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class MyTankGame4 extends JFrame{
+
+	MyPanel2 mp=null;
+	MyStartPanel msp=null;
+	public static void main(String[] args) {
+		//在这里写启动代码
+		MyTankGame4 mtg = new MyTankGame4();
+	}
+	//公共类的构造函数
+	public MyTankGame4()
+	{
+		/*
+		//在构造函数里，调用其他的类代码，其他类一般都是业务代码
+		//调用自定义MyPanel2类，启动时会自动调用其paint方法
+		mp = new MyPanel2();
+		
+		//启动面板进程
+		Thread t = new Thread(mp);
+		t.start();
+		
+		//为当前窗体添加面板
+		this.add(mp);
+	*/	
+		msp=new MyStartPanel();
+		Thread mspt= new Thread(msp);
+		mspt.start();
+		this.add(msp);
+		//为当前窗体添加事件监听者，巧的是，事件监听者，也是这个窗体里的面板。
+		//任何对象都可以添加事件监听者，应该针对对象的操作特性添加对应的事件监听者
+		//面板里我的坦克的移动，发射子弹，都是通过按键来实现的，故添加（键盘）事件监听者，以监听面板对象里按键的操作
+		//this.addKeyListener(mp);
+		
+		
+		this.setSize(400,300);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+}
+
+//提示当前是第几关，就是一个初始的Panel而已
+//加上闪烁效果，那就得做成线程 
+class MyStartPanel extends JPanel implements Runnable
+{
+	//定义一个开关
+	int times=0;
+	public void paint(Graphics g){
+		super.paint(g);
+		//给出提示语就行,设置颜色与字体
+		g.fillRect(0, 0, 400, 300);
+		g.setColor(Color.YELLOW);
+		if(times%2==0) {
+			Font myfont=new Font("华文新魏",Font.BOLD,30);
+			g.setFont(myfont);
+			g.drawString("Stage 1", 150, 150);
+		}
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		while(true) {
+			//休眠
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			times++;
+			this.repaint();
+		}
+		
+	}
+}
+
+//面板类 在其中画很多的坦克对象
+class MyPanel2 extends JPanel implements KeyListener ,Runnable
+{
+	//面板里，定义一个坦克
+	Hero hero = null;
+	
+	//定义三张图片，三张图片组成坦克消失的爆炸效果（图片按照一定顺序与频率切换，欺骗人眼）
+	Image image1 = null;
+	Image image2 = null;
+	Image image3 = null;
+	//爆炸对象集合，每个爆炸对象各自处理那三张图片的切换
+	Vector<Bomb> vbs = new Vector<Bomb>();
+	
+	//敌人的坦克，用集合
+	Vector<Enemy> ves = new Vector<Enemy>();
+	
+	//初始化时控制敌人坦克的数量
+	int enSize = 6;
+	
+	//构造方法 只有修饰符
+	public MyPanel2()
+	{
+		//初始化我的坦克，就一辆，直接new一次完事
+		hero = new Hero(60,100);
+		
+		//敌军坦克不少，需要循环new出来，放到集合对象里
+		for(int i=0;i<enSize;i++) {
+			//创建一辆敌人坦克（面板上部，y轴固定为0，x轴往右一字排开）
+			Enemy enemy = new Enemy((i+1)*50,0);
+			//每个敌人坦克，也是一个进程
+			Thread t = new Thread(enemy);
+			t.start();
+			
+			//加入到当前坦克的队友向量中,使得当前坦克可以判断周围队友的情况
+			//虽然第一次加时，是空坦克，但是ves是引用，故最终每个坦克都有唯一的一个引用集合
+			//包含了所有敌人的坦克，包括自己在内
+			enemy.setVes(ves);
+			
+			//给敌人坦克添加一颗子弹,放到它的子弹集合里
+			Shot s=new Shot(enemy.x,enemy.y,enemy.direct);
+			enemy.vs.add(s);
+			//它的子弹也是一个线程
+			Thread ts= new Thread(s);
+			ts.start();
+			
+			//加入到敌人坦克集合中
+			ves.add(enemy);
+		}
+		//首先引入这三张爆炸效果图
+		try {
+			//注意，图片文件路径不是在包src内，而是在项目Class9目录下
+			image1=ImageIO.read(new File("bomb_1.gif"));
+			image2=ImageIO.read(new File("bomb_2.gif"));
+			image3=ImageIO.read(new File("bomb_3.gif"));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * 遍历我的子弹，再遍历敌人的坦克
+	 * 从而去判断是否击中敌人坦克。
+	 */
+	public void TraverseMyShotForEnemy()
+	{
+		//在这里，时时刻刻判断，面板里的子弹是否击中了敌人坦克
+		//遍历子弹，每个子弹再遍历敌人坦克
+		for (int i=0;i<hero.vs.size();i++) {
+			//取出子弹
+			Shot myShot = hero.vs.get(i);
+			//子弹是否有效（想到上次线程完全的问题，子弹本身就是资源）
+			if (myShot.isLive) {
+				//再循环敌人坦克
+				for (int j=0;j<ves.size();j++) {
+					//取出一个坦克
+					Enemy et = ves.get(j);
+					if (et.isLive) {
+						//是否击中坦克，单独写个方法，传入子弹和坦克对象
+						this.hitTank(myShot,et);
+					}
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 遍历敌人的坦克，再遍历每个坦克的子弹，查看我的坦克是否被击中
+	 */
+	private void traverseEnemyShotIsMe()
+	{
+		//遍历敌人坦克，再遍历该坦克的子弹
+		for (int i=0;i<ves.size();i++) {
+			//取出敌人坦克
+			Enemy e = ves.get(i);
+			//循环这个坦克的子弹
+			for(int j=0;j<e.vs.size();j++){
+				Shot eShot = e.vs.get(j);
+				//子弹是否有效（想到上次线程完全的问题，子弹本身就是资源）
+				if (eShot.isLive) {
+					if (hero.isLive) {//我的坦克也还有效
+						//是否击中坦克，单独写个方法（修改最初判断是否击中敌人坦克的方法而来）
+						this.hitTank(eShot,hero);
+					}
+				}
+			}
+		}
+	}
+	
+	
+	//写一个方法，判断子弹是否击中敌人坦克
+	//原理就是，只要子弹的坐标，在敌人坦克坐标的范围内，即为击中坦克。
+	//但是，坦克始终是左上角为参考点，且坦克的坐标范围，随方向的不同，计算不同，故先需判断方向
+	//扩展这个函数，也可以判断是否击中我的坦克，故升级第二个参数类型为Tank
+	private void hitTank(Shot s,Tank et)
+	{
+		//判断方向
+		switch(et.direct)
+		{
+			case 0://上，下朝向的
+			case 2:
+				if(s.x>et.x && s.x<et.x+20 && s.y>et.y && s.y<et.y+30) {
+					//击中
+					//子弹消失
+					s.isLive = false;
+					//敌人坦克消失
+					et.isLive = false;
+					//击中坦克，就要产生爆炸效果,放入集合中
+					Bomb b=new Bomb(et.x,et.y);
+					vbs.add(b);
+				}
+				break;
+			case 1://左右朝向的
+			case 3:
+				if(s.x>et.x && s.x<et.x+30 && s.y>et.y && s.y<et.y+20) {
+					//击中
+					s.isLive = false;
+					//敌人坦克消失
+					et.isLive = false;
+					//击中坦克，就要产生爆炸效果,放入集合中
+					Bomb b=new Bomb(et.x,et.y);
+					vbs.add(b);
+				}
+		}
+	}
+	
+	
+	//JPanel重要的方法paint
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		//首先把整个面板使用默认颜色
+		g.fillRect(0, 0, 400, 300);
+		//画自己坦克，方向传入，不再是固定的0，方向是按键决定的，动态地
+		//既然我的坦克可以被敌人击中，那么我的坦克活着才可以被画出来
+		if(hero.isLive) {
+			this.drawTank(hero.getX(), hero.getY(), g, hero.direct, 1);
+		}else{
+			//hero=null;
+		}
+		
+		//这里得修改了，不能只画出一个子弹去paint，而应该使用循环，因为我的坦克可以发送多颗子弹
+		for(int i=0;i<hero.vs.size();i++) {
+			Shot myshot = hero.vs.get(i);
+			if(myshot != null && myshot.isLive) {
+				g.draw3DRect(myshot.x,myshot.y,1,1,false);
+			}
+			if(myshot.isLive == false) {
+				//从vs中删除该子弹
+				//注意，不要使用i而应该使用myshot。
+				hero.vs.remove(myshot);
+			}
+		}
+		
+		//画出击中图片,即爆炸效果，实现原理就是：一旦有坦克爆炸就实例化一个爆炸类，记住爆炸位置坐标
+		//然后在该位置上按照顺序和频率切换这三张图片而已
+		for(int i=0;i<vbs.size();i++) {
+			Bomb b=vbs.get(i);
+			if(b.life>6){
+				g.drawImage(image1, b.x, b.y, 30,30,this);
+			}else if(b.life>3){
+				g.drawImage(image2, b.x, b.y, 30,30,this);
+			}else{
+				g.drawImage(image3, b.x, b.y, 30,30,this);
+			}
+			//生命力减少
+			b.lifeDown();
+			//生命力为0时，从向量中删除即可
+			if(!b.isLive){
+				vbs.remove(b);
+			}
+		}
+		
+		//循环画出敌人坦克
+		for(int i=0;i<ves.size();i++) {
+			Enemy et=ves.get(i);
+			//判断是否这个坦克还活着
+			if(et.isLive) {
+				this.drawTank(et.getX(), et.getY(), g, et.direct, 0);
+				//顺便画出它的子弹
+				for(int j=0;j<et.vs.size();j++){
+					Shot s= et.vs.get(j);
+					//如果它的子弹还有效
+					if(s.isLive) {
+						g.draw3DRect(s.x,s.y,1,1,false);
+					}else{
+						//否则就清除它的子弹
+						et.vs.remove(s);
+					}
+				}
+			} else {
+				ves.remove(et);//敌人的坦克消失
+				//System.out.println("敌人坦克数量 2="+ves.size());
+			}
+			//System.out.println("敌人坦克数量="+ves.size());
+		}
+	}
+	//画出一个坦克的方法
+	/**
+	 * 画出坦克的方法
+	 * @param x 初始化坦克时的x坐标
+	 * @param y 初始化坦克时的y坐标
+	 * @param g 不清楚
+	 * @param direction 初始化时坦克的朝向
+	 * @param type  坦克类型  （1我的坦克  0敌军坦克）
+	 */
+	public void drawTank(int x,int y,Graphics g,int direction,int type)
+	{
+		//判断敌我坦克的颜色不同
+		switch(type)
+		{
+			case 0:
+				g.setColor(Color.cyan);
+				break;
+			case 1:
+				g.setColor(Color.yellow);
+				break;
+		}
+		
+		//判断坦克方向，需要纸上画出图来，才能好理解
+		//向上和向右，其实就是矩阵的转换（x,y交互是什么来着？）
+		switch(direction)
+		{
+			//向上 （炮筒向上） 
+			case 0:
+				//画出我的坦克(需要先在纸上画出，左轮，右轮，坦克矩形，坦克头，坦克炮头五个小组件
+				//才能看明白下述的五个步骤）
+				//1 画出左边的矩形
+				g.fill3DRect(x, y, 5, 30,false);
+				// 2 画出右边矩形，往右偏移15公分
+				g.fill3DRect(x+15, y, 5, 30,false);
+				// 3 画出中间矩形
+				g.fill3DRect(x+5, y+5, 10, 20,false);
+				// 4 画出中间圆形
+				g.fillOval(x+5, y+10, 8, 8);
+				//5 画出炮筒（直线）
+				g.drawLine(x+8, y+10, x+8, y);
+				break;
+				
+				//向右  炮筒向右
+			case 1:
+				//1 画出上边的矩形
+				g.fill3DRect(x, y, 30, 5,false);
+				// 2 画出下边矩形，往右偏移15公分
+				g.fill3DRect(x, y+15, 30, 5,false);
+				// 3 画出中间矩形
+				g.fill3DRect(x+5, y+5, 20, 10,false);
+				//4 画出中间圆形
+				g.fillOval(x+10, y+5, 8, 8);
+				//5 画出炮筒（直线）
+				g.drawLine(x+10, y+8, x+30, y+8);
+				break;
+				
+			//向下（与向上的区别，就是炮筒的方向而已）
+			case 2:
+				//画出我的坦克(需要先在纸上画出，左轮，右轮，坦克矩形，坦克头，坦克炮头五个小组件）
+				//1 画出左边的矩形
+				g.fill3DRect(x, y, 5, 30,false);
+				// 2 画出右边矩形，往右偏移15公分
+				g.fill3DRect(x+15, y, 5, 30,false);
+				// 3 画出中间矩形
+				g.fill3DRect(x+5, y+5, 10, 20,false);
+				// 4 画出中间圆形
+				g.fillOval(x+5, y+10, 8, 8);
+				//5 画出炮筒（直线）
+				g.drawLine(x+8, y+10, x+8, y+30);
+				break;
+				
+				//向左
+			case 3:
+				//1 画出上边的矩形
+				g.fill3DRect(x, y, 30, 5,false);
+				// 2 画出下边矩形，往右偏移15公分
+				g.fill3DRect(x, y+15, 30, 5,false);
+				// 3 画出中间矩形
+				g.fill3DRect(x+5, y+5, 20, 10,false);
+				//4 画出中间圆形
+				g.fillOval(x+10, y+5, 8, 8);
+				//5 画出炮筒（直线）
+				g.drawLine(x+10, y+8, x, y+8);
+				break;
+
+		}
+	}
+
+
+	/**
+	 * 
+	 * 以事件监听者，分别判断按键，来达到方向和速度的控制
+	 * w上	d右	s下	a左	
+	 * 这是面板作为事件监听者（按键事件）必须实现的方法（多个方法）
+	 */
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyCode() == KeyEvent.VK_W) {
+			this.hero.setDirect(0);
+			this.hero.moveUp();
+		} else if(e.getKeyCode() == KeyEvent.VK_D) {
+			this.hero.setDirect(1);
+			this.hero.moveRight();
+		} else if(e.getKeyCode() == KeyEvent.VK_S) {
+			this.hero.setDirect(2);
+			this.hero.moveDown();
+		} else if(e.getKeyCode() == KeyEvent.VK_A) {
+			this.hero.setDirect(3);
+			this.hero.moveLeft();
+		}
+		
+		//单独拿个分支来判断我的坦克发射子弹的按键事件处理
+		if (e.getKeyCode() == KeyEvent.VK_J) {
+			//子弹如果连续发射就太霸道了，所以我们可以只让它最多五发子弹
+			if(hero.vs.size()<=4) {
+				this.hero.shotEnemy();
+			}
+		}
+		
+		//在更新了面板里的元素，元素坐标位置，大小，颜色等后，重新渲染
+		//不断地按键，不断地重新渲染(repaint()方法的底层其实还是会调用paint()，但是我们已经不能再次调用paint()）
+		//this.repaint();
+	}
+
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 面板作为一个进程，必须实现run方法
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//每隔100毫秒，去重新渲染。
+		//最初的渲染，是通过事件监听者来实现的，在事件监听者方法的最后，通过this.repaint()来重新渲染的
+		//这种渲染，只有触发了按键才能渲染，不触发就不渲染，因为当时尚不能发射子弹，仅是画出坦克，
+		//坦克可以上下左右移动，仅此而已。这些都是按键后才会进行的操作效果，不按键就不动，故重新渲染和触发按键
+		//两个逻辑在一块
+		//而这里使用进程方式让面板自动渲染，使之脱离了事件触发的限制
+		//一旦启动面板进程，就一直渲染，无论事件是否触发。因为开发到现在，面板里已经有坦克了，
+		//重点是坦克可以发射子弹了，子弹的位置变化，击中坦克，敌人坦克的移动，都得实时渲染，才能看到效果。
+		while(true)
+		{
+			try {
+				Thread.sleep(100);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+			this.TraverseMyShotForEnemy();
+			//写个函数，敌人的子弹是否击中我的坦克
+			this.traverseEnemyShotIsMe();
+			//渲染
+			this.repaint();
+		}
+	}
+}
