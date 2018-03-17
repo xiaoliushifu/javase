af53b8aa0670ff2a89c367b497a687ee710d88f7
liushifu <872140945@qq.com>
2018-03-17 23:58:01 +0800

    struts.xml配置文件说明 这是核心，它和struts的jar包里的类有直接关系 常量配置，哪些常量 继承struts-default。继承了好多配置 总之，学习了struts.xml的配置，你就基本掌握了struts2这个控制器框架

--- struts/src/com/liu/struts/actions/LmwHelloAction.java
+++ struts/src/com/liu/struts/actions/LmwHelloAction.java
@@ -10,13 +10,24 @@ public class LmwHelloAction extends ActionSupport{
 	//execute
 	/* (non-Javadoc)
 	 * @see com.opensymphony.xwork2.ActionSupport#execute()
+	 * struts.xml的一个<action>的method属性默认就是execute
+	 * 所以该execute方法与struts.xml中没有写method属性的<action>映射
 	 */
 	@Override
 	public String execute() throws Exception {
-		// TODO Auto-generated method stub
 		System.out.println("hello 明伟啊");
-		//return super.execute();
 		return "success";
+	}
+	
+	/**
+	 * 自定义order方法
+	 * 该方法就对应struts.xml中的一个<action method="order" xxx>
+	 * @return String
+	 * 返回类型必须是String
+	 */
+	public String order(){
+		System.out.println("下订单成功2");
+		return "orderError";
 	}
 
 }
