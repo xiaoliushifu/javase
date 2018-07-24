dd2e0ef7952063471b9093f906734fa27ae173cf
liushifu <872140945@qq.com>
2018-07-24 09:46:53 +0800

    提交下composer.json文件

--- put.php
+++ put.php
@@ -1,9 +1,15 @@
 <?php
 require 'vendor/autoload.php';
 
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
 use Elasticsearch\ClientBuilder;
 
-$client = ClientBuilder::create()->build();
+#准备日志组件
+$logger = new Logger('name');
+$logger->pushHandler(new StreamHandler('path/to/your.log', Logger::WARNING));
+
+$client = ClientBuilder::create()->setLogger($logger)->build();
 
 #创建一个索引文档，按照sdk的说法，这些都是关联数组
 $params = [
